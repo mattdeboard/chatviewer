3316afa8a37707bf30d2e0d8f0c2fbfd08698505
Matt DeBoard <matt.deboard@gmail.com>
2015-03-03 18:21:51 -0600

    Flesh out TopicPalette component.

---
+++ src/js/components/topic_palette.jsx
@@ -0,0 +1,50 @@
+const React = require('react');
+const _ = require('underscore');
+
+const TopicPalette = React.createClass({
+  propTypes: {
+    numTopics: React.PropTypes.number
+  },
+
+  getInitialState: function() {
+    return {
+      hidden: []
+    };
+  },
+
+  handleClick: function(e) {
+    return;
+  },
+
+  handleKeyPress: function(e) {
+    if (["Space", "Enter"].indexOf(e.key) !== -1) {
+      return this.handleClick(e);
+    }
+  },
+  
+  renderTopicLinks: function() {
+    return _.range(this.props.numTopics).map(function(i, idx, this) {
+      return (
+        <button className="btn btn-default"
+                tabIndex={i + 1}
+                key={"topic-link-button-" + i}
+                onClick={this.handleClick}
+                onKeyUp={this.handleKeyPress}
+                onKeyDown={this.handleKeyPress}>
+          Topic {i}
+        </button>
+      );
+    })
+  },
+
+  render: function() {
+    return (
+      <div className="palette" id="topic-palette">
+        {this.renderTopicLinks()}
+      </div>
+    );
+  }
+
+});
+
+module.exports = TopicPalette;
